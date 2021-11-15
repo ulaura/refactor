60d7564f6b10927fc7f61c4bc272c8a1f0819e5f
Laura Unaeze <l.unaeze5@gmail.com>
2021-11-14 20:35:49 -0700

    feat(hotelCard): moved hotel cards to its own component

--- src/App.js
+++ src/App.js
@@ -9,12 +9,62 @@ import Container from 'react-bootstrap/Container'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import Row from 'react-bootstrap/Row'
-import hotels from './hotels';
+// import hotels from './hotels';
 
 function App() {
   const [hotel1Color, setHotel1Color] = useState('text-primary');
   const [hotel2Color, setHotel2Color] = useState('text-primary');
   const [hotel3Color, setHotel3Color] = useState('text-primary');
+
+
+  const [hotelColor, setHotelColor] = useState({
+    0: {
+      color: 'text-primary'
+    },
+    1: {
+      color: 'text-primary'
+    },
+    2: {
+      color: 'text-primary'
+    },
+  });
+
+  const hotels = [
+    {
+      id: 0,
+      brandId: '12345',
+      name: 'Boardwalk Hotel',
+      color: hotelColor["0"].color,
+    },
+    {
+      id: 1,
+      brandId: '8675309',
+      name: 'The Jenny Hotel & Suites',
+      color: hotelColor["1"].color,
+    },
+    {
+      id: 2,
+      brandId: 'ABC123',
+      name: 'Alpha Numeric Inn',
+      color: hotelColor["2"].color,
+    }
+  ]
+
+  const changeHotelColorPrimary = (id) => {
+    setHotelColor({
+      ...hotelColor,
+      [id]: {color: 'text-primary'},
+    });
+  }
+
+  
+  const changeHotelColorSecondary = (id) => {
+    setHotelColor({
+      ...hotelColor,
+      [id]: {color: 'text-secondary'},
+    });
+  }
+
 
   const options = {
     title: {
@@ -25,56 +75,47 @@ function App() {
     }]
   }
 
+
+
+  
+
+
+  const HotelCard = ({hotelName, key, id, cardColor}) => {
+    return (
+      <React.Fragment key={key}>
+        <Col>
+          <Card>
+            <Card.Header>
+              {hotelName} 
+              <Button size='sm' variant='primary' onClick={() => changeHotelColorPrimary(id)}>Primary Color</Button>
+              <Button size='sm' variant='secondary' onClick={() => changeHotelColorSecondary(id)}>Secondary Color</Button>
+            </Card.Header>
+            <Card.Body className={cardColor}>
+              <Card.Title>Welcome to the lovely {hotelName}!</Card.Title>
+              <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+      </React.Fragment>
+    );
+  }
+
   return (
     <Container>
       <Card>
         <Card.Header>Welcome To Your Dashboard</Card.Header>
         <Row>
-          <Col>
-            <Card>
-              <Card.Header>{hotels[0].name} 
-                <Button size='sm' variant='primary' onClick={() => setHotel1Color('text-primary')}>Primary Color</Button>
-                <Button size='sm' variant='secondary' onClick={() => setHotel1Color('text-secondary')}>Secondary Color</Button>
-              </Card.Header>
-              <Card.Body className={hotel1Color}>
-                <Card.Title>Welcome to the lovely {hotels[0].name}!</Card.Title>
-                <HighchartsReact
-                  highcharts={Highcharts}
-                  options={options}
-                />
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Header>{hotels[1].name}
-                <Button size='sm' variant='primary' onClick={() => setHotel2Color('text-primary')}>Primary Color</Button>
-                <Button size='sm' variant='secondary' onClick={() => setHotel2Color('text-secondary')}>Secondary Color</Button>
-              </Card.Header>
-              <Card.Body className={hotel2Color}>
-              <Card.Title>Welcome to the lovely {hotels[1].name}!</Card.Title>
-                <HighchartsReact
-                  highcharts={Highcharts}
-                  options={options}
-                />
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Header>{hotels[2].name}
-                <Button size='sm' variant='primary' onClick={() => setHotel3Color('text-primary')}>Primary Color</Button>
-                <Button size='sm' variant='secondary' onClick={() => setHotel3Color('text-secondary')}>Secondary Color</Button>
-              </Card.Header>
-              <Card.Body className={hotel3Color}>
-              <Card.Title>Welcome to the lovely {hotels[2].name}!</Card.Title>
-                <HighchartsReact 
-                  highcharts={Highcharts}
-                  options={options}
-                />
-              </Card.Body>
-            </Card>
-          </Col>
+          {hotels.map(hotel => (
+            <HotelCard 
+              key={hotel.id}
+              id={hotel.id}
+              hotelName={hotel.name}
+              cardColor={hotel.color} 
+            />
+         ))}
         </Row>
       </Card>
     </Container>
