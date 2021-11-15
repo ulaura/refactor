61b704b3b2f67b1f87c74ffc2a0785ea6d25ddd0
Laura Unaeze <l.unaeze5@gmail.com>
2021-11-14 23:10:47 -0700

    feat(hotelCard): moved state so it wouldn't cause big re-renders

--- src/App.js
+++ src/App.js
@@ -12,75 +12,70 @@ import Row from 'react-bootstrap/Row'
 // import hotels from './hotels';
 
 function App() {
-  const [hotel1Color, setHotel1Color] = useState('text-primary');
-  const [hotel2Color, setHotel2Color] = useState('text-primary');
-  const [hotel3Color, setHotel3Color] = useState('text-primary');
 
-
-  const [hotelColor, setHotelColor] = useState({
-    0: {
-      color: 'text-primary'
-    },
-    1: {
-      color: 'text-primary'
-    },
-    2: {
-      color: 'text-primary'
-    },
-  });
-
+  // make sure the id's here matches the id in useState for <HotelCard/>
   const hotels = [
     {
       id: 0,
       brandId: '12345',
       name: 'Boardwalk Hotel',
-      color: hotelColor["0"].color,
     },
     {
       id: 1,
       brandId: '8675309',
       name: 'The Jenny Hotel & Suites',
-      color: hotelColor["1"].color,
     },
     {
       id: 2,
       brandId: 'ABC123',
       name: 'Alpha Numeric Inn',
-      color: hotelColor["2"].color,
     }
   ]
 
-  const changeHotelColorPrimary = (id) => {
-    setHotelColor({
-      ...hotelColor,
-      [id]: {color: 'text-primary'},
+
+  const HotelCard = ({hotelName, key, id}) => {
+    // puting the hotelColor state inside HotelCard
+    // makes the color state change w/o re-rendering the entire app.
+    const [hotelColor, setHotelColor] = useState({
+      0: {
+        color: 'text-primary'
+      },
+      1: {
+        color: 'text-primary'
+      },
+      2: {
+        color: 'text-primary'
+      },
     });
-  }
-
   
-  const changeHotelColorSecondary = (id) => {
-    setHotelColor({
-      ...hotelColor,
-      [id]: {color: 'text-secondary'},
-    });
-  }
-
-
-  const options = {
-    title: {
-      text: 'My chart'
-    },
-    series: [{
-      data: [1, 2, 3]
-    }]
-  }
-
-
-
   
-
-
-  const HotelCard = ({hotelName, key, id, cardColor}) => {
+    const changeHotelColorPrimary = (id) => {
+      setHotelColor({
+        ...hotelColor,
+        [id]: {color: 'text-primary'},
+      });
+    }
+  
+    
+    const changeHotelColorSecondary = (id) => {
+      setHotelColor({
+        ...hotelColor,
+        [id]: {color: 'text-secondary'},
+      });
+    }
+  
+  
+    const options = {
+      title: {
+        text: 'My chart'
+      },
+      series: [{
+        data: [1, 2, 3]
+      }]
+    }
+  
+  
+  
     return (
       <React.Fragment key={key}>
         <Col>
@@ -90,7 +85,7 @@ function App() {
               <Button size='sm' variant='primary' onClick={() => changeHotelColorPrimary(id)}>Primary Color</Button>
               <Button size='sm' variant='secondary' onClick={() => changeHotelColorSecondary(id)}>Secondary Color</Button>
             </Card.Header>
-            <Card.Body className={cardColor}>
+            <Card.Body className={hotelColor[id].color}>
               <Card.Title>Welcome to the lovely {hotelName}!</Card.Title>
               <HighchartsReact
                 highcharts={Highcharts}
@@ -113,7 +108,6 @@ function App() {
               key={hotel.id}
               id={hotel.id}
               hotelName={hotel.name}
-              cardColor={hotel.color} 
             />
          ))}
         </Row>
